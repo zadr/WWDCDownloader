cf8d8a2ebc5240d4adf5b20822ab44d237fa9507
Zach Drayer <zach@drayer.name>
2013-06-16 02:42:18 -0700

    Less duplicate code

--- WWDCDownloader/DOMIteration.m
+++ WWDCDownloader/DOMIteration.m
@@ -7,34 +7,29 @@
 
 #import "DOMIteration.h"
 
+#define iterate \
+	do { \
+		BOOL stop = NO; \
+ \
+		for (unsigned i = 0; i < self.length; i++) { \
+			id item = [self item:i]; \
+ \
+			block(item, i, &stop); \
+ \
+			if (stop) { \
+				break; \
+			} \
+		} \
+	} while (0)
+
 @implementation DOMNodeList (Additions)
 - (void) enumerateObjectsUsingBlock:(void (^)(DOMObject *obj, unsigned idx, BOOL *stop)) block {
-	BOOL stop = NO;
-
-	for (unsigned i = 0; i < self.length; i++) {
-		id item = [self item:i];
-
-		block(item, i, &stop);
-
-		if (stop) {
-			break;;
-		}
-	}
+	iterate;
 }
 @end
 
 @implementation DOMHTMLCollection (Additions)
 - (void) enumerateObjectsUsingBlock:(void (^)(DOMHTMLElement *obj, unsigned idx, BOOL *stop)) block {
-	BOOL stop = NO;
-
-	for (unsigned i = 0; i < self.length; i++) {
-		id item = [self item:i];
-
-		block(item, i, &stop);
-
-		if (stop) {
-			break;;
-		}
-	}
+	iterate;
 }
 @end
